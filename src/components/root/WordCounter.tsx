77882974a678499ec7928932846724cc793a0f14
Angelo Filaseta <filaseta.angelo@gmail.com>
2023-04-03 20:08:09 +0200

    feat: add counter logic

--- src/components/root/WordCounter.tsx
+++ src/components/root/WordCounter.tsx
@@ -1,10 +1,21 @@
+import {useEffect, useState} from "react";
+import {Concept} from "../../rdf/types/Concept";
+
 export default function WordCounter() {
+    const [wordCount, setWordCount] = useState(0)
+
+    useEffect(() => {
+        Concept.all().then(concepts => {
+            setWordCount(concepts.length)
+        })
+    }, [])
+
     return (
         <>
             <section className="spotlight style1 orient-right content-align-left image-position-center onscroll-image-fade-in"
                      id="first">
                 <div className="content">
-                    <h1> TODO </h1>
+                    <h1> {wordCount} </h1>
                     <p>Sono le parole all'interno del Vocabolangelo. Puntiamo a raggiungere le 1000 parole.</p>
                     <ul className="actions stacked">
                         <li>
