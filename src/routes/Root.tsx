229256c0d86fc0f796c3fa033a0224d80cadfdfa
Angelo Filaseta <filaseta.angelo@gmail.com>
2023-04-24 17:34:07 +0200

    feat: add disclaimer

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -11,6 +11,25 @@ export default function Root() {
     return (
         <>
             <Header/>
+            <section className="banner style3 orient-left content-align-left image-position-right fullscreen onload-image-fade-in onload-content-fade-right">
+                <div className="content">
+                    <h1>ATTENZIONE</h1>
+                    <p className="major">
+                        Il Vocabolangelo è momentaneamente sotto <strong>manutenzione</strong>.<br/>
+                        Al momento il sito non è stabile e potrebbe presentare diversi bug.<br/>
+                    </p>
+                    <ul className="actions stacked">
+                        <li>
+                            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="button big wide smooth-scroll-middle">
+                                Il gioco
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="image">
+                    <img src="/images/danger.png" alt="Pericolo"/>
+                </div>
+            </section>
             <Disclaimer/>
             <Introduction/>
             <WordCounter/>
