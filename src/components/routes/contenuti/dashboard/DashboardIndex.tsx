9df3c7ca571a685ca2e506284693bb13ec6444a6
Angelo Filaseta <filaseta.angelo@gmail.com>
2023-06-11 12:57:36 +0200

    feat(site): add graph component

--- src/components/routes/contenuti/dashboard/DashboardIndex.tsx
+++ src/components/routes/contenuti/dashboard/DashboardIndex.tsx
@@ -2,6 +2,7 @@ import ParolangeloCountSpotlight from './ParolangeloCountSpotlight'
 import CreatorCountSpotlight from './CreatorCountSpotlight'
 import LeaderBoard from './LeaderBoard'
 import {Main} from '../../../common/Main'
+import MonthWords from './MonthWords'
 
 export const DASHBOARD_ROUTE = '/dashboard'
 
@@ -9,6 +10,7 @@ export default function DashboardIndex() {
     return <Main>
         <ParolangeloCountSpotlight/>
         <CreatorCountSpotlight/>
+        <MonthWords/>
         <LeaderBoard/>
     </Main>
 }
